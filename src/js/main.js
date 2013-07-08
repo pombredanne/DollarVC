4a8c02ed01b6eb04a0fd70716b7a5574ca1f261a
XiZhao <kwang1imsa@gmail.com>
2013-07-07 23:14:22 -0700

    Initial Commit

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+var Content
+{
+    Perks: [
+        'Networks': [
+        {
+            title: 'Thiel Fellowship',
+            link: 'http://www.thielfellowship.org',
+            content: 'DollarVC was started by Thiel Fellows, and we have access to the entire mentorship network of 250 successful founders, VCs and fellows. Gain intros, expertise and funds as a portfilio company.',
+            perks: [{
+                icon: 'bitbucket',
+                text: 'I know the founder of bitbucket'
+            }, 'perk', 'perk']
+        }],
+        'Discounts & Deals': [ //partnerships?
+        {
+            title: '',
+            link: '',
+            content: '',
+            perks: []
+        }],
+        'Venture Capital': [
+        {
+            title: 'XSeedCapital',
+            link: 'http://www.xseedcap.com/',
+            content: '',
+            perks: []
+        }]
+    ]
+}
+ko.applyBindings(Content);
